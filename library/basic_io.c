0b6c057da9233f21f6db11d953ff223d3918c9d2
amiq <amiq@d79e39b7-3be6-4885-810b-d6779ea70ea7>
2011-06-05 06:28:09 +0000

    added accel
    git-svn-id: https://robotich.ath.cx/svn/circuit/misc/shimazu/semi/trunk@12942 d79e39b7-3be6-4885-810b-d6779ea70ea7

--- library/basic_io.c
+++ library/basic_io.c
@@ -2,6 +2,25 @@
 
 #include "library.h"
 
+void init_sw(void){
+    pin_in_init(SW0_DDR,SW0_PIN);
+    set_pin_hi(SW0_PORT,SW0_PIN);
+}
+    
+
+/* スイッチを読み取る */
+/* hardware.hにあるPOS,NEGの設定により0,1が反転する */
+uint8_t get_sw(uint8_t val){
+    switch(val){
+    case 0:
+#if SW0_DIR==POS
+        return (read_pin(SW0_REG,SW0_PIN)==0?0:1);
+#else
+        return (read_pin(SW0_REG,SW0_PIN)==0?1:0);
+#endif
+    }
+    return 0xFF;
+}
 
 
 
