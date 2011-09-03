d75e3ed60406dd8cd6c0c3cf067e82cd872a3c10
amiq <amiq@d79e39b7-3be6-4885-810b-d6779ea70ea7>
2011-09-03 09:21:39 +0000

    間違えてコミットした分を逆マージ
    git-svn-id: https://robotich.ath.cx/svn/semi11s/lecture_circuit/firmware/trunk@15098 d79e39b7-3be6-4885-810b-d6779ea70ea7

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 /* main.c */
 #include "library.h"
 #include "api.h"
-#include <util/delay.h>
 #include <avr/eeprom.h>
 #include <math.h>
 
@@ -14,86 +13,75 @@
 int main(void){
 
     init();                     /* 初期化 */
-    /* init_adc(ADC3D);            /\* ジョイスティック *\/ */
-    /* init_adc(ADC4D); */
-    /* sei();                      /\* 割り込み許可 *\/ */
+    init_adc(ADC3D);            /* ジョイスティック */
+    init_adc(ADC4D);
+    sei();                      /* 割り込み許可 */
 
 
     /* 本文 */
-    /* printf("_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n" */
-    /*        "Semi in 2011 Summer\n" */
-    /*        "Sample Program\n" */
-    /*        "by Shimazu Makoto\n" */
-    /*        "Date: 11-06-02\n" */
-    /*        "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n"); */
-    /* load_accel_horizontal(); */
+    printf("_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n"
+           "Semi in 2011 Summer\n"
+           "Sample Program\n"
+           "by Shimazu Makoto\n"
+           "Date: 11-06-02\n"
+           "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n");
+    load_accel_horizontal();
 
-    pin_out_init(DDRB,PB0);
-    
     /* メインループ */
     while(1){
-        /* for(int i=1;i<10;i++){ */
-            set_pin_low(PORTB,PB0);
-            _delay_us(5.8);
-            set_pin_hi(PORTB,PB0);
-            /* delay_us(10); */
-        /* } */
-        /* delay_ms(100); */
+        static uint8_t old_sw[8]={0};
+        int16_t buf[4]={0};
+        uint8_t id;
+        
+        if(pushed_on(0))
+            save_accel_horizontal();
+        
+        save_sw_condition(0);
+        
+
+        buf[X]=get_accel(X);
+        buf[Y]=get_accel(Y);
+        buf[Z]=get_accel(Z);
+        /* buf[X]=read_adc(ADC3D); */
+        /* buf[Y]=read_adc(ADC4D); */
+        /* buf[Z]=0; */
+        
+        
+        /* buf[X]=get_accel_direct(X); */
+        /* buf[Y]=get_accel_direct(Y); */
+        /* buf[Z]=get_accel_direct(Z); */
+        
+
+        
+        if(buf[X]<0){
+            id=3-(uint8_t)(abs(buf[X])/50);
+        }else{
+            id=4+(uint8_t)(abs(buf[X])/50);
+        }
+
+        
+        for(uint8_t i=0;i<8;i++){
+            if(i==id)
+                set_led(i,255);
+            else
+                set_led(i,0);
+        }
+
+        set_color_led((buf[X]>>1)&0xFF ,(buf[Y]>>1)&0xFF,(buf[Z]>>1)&0xFF);
+        
+        /* printf("%5d %5d %5d LED:%d\n",abs((buf[X]*2))%255,abs((buf[Y]*2))%255,abs((buf[Z]*2))%255,id); */
+        
+        
+        printf("%5d %5d %5d\n",
+               buf[X],buf[Y],buf[Z]);
+        /* printf("\033[%dA" ,1); //カーソルを上に移動 */
+        
+        /* set_color_led(buf[X]+0x7F,buf[Y]+0x7F,buf[Z]+0x7F); */
+        
+        
+
+        delay_ms(10);
     }
-            
-        /* static uint8_t old_sw[8]={0}; */
-        /* int16_t buf[4]={0}; */
-        /* uint8_t id; */
-        
-        /* if(pushed_on(0)) */
-        /*     save_accel_horizontal(); */
-        
-        /* save_sw_condition(0); */
-        
-
-        /* buf[X]=get_accel(X); */
-        /* buf[Y]=get_accel(Y); */
-        /* buf[Z]=get_accel(Z); */
-        /* /\* buf[X]=read_adc(ADC3D); *\/ */
-        /* /\* buf[Y]=read_adc(ADC4D); *\/ */
-        /* /\* buf[Z]=0; *\/ */
-        
-        
-        /* /\* buf[X]=get_accel_direct(X); *\/ */
-        /* /\* buf[Y]=get_accel_direct(Y); *\/ */
-        /* /\* buf[Z]=get_accel_direct(Z); *\/ */
-        
-
-        
-        /* if(buf[X]<0){ */
-        /*     id=3-(uint8_t)(abs(buf[X])/50); */
-        /* }else{ */
-        /*     id=4+(uint8_t)(abs(buf[X])/50); */
-        /* } */
-
-        
-        /* for(uint8_t i=0;i<8;i++){ */
-        /*     if(i==id) */
-        /*         set_led(i,255); */
-        /*     else */
-        /*         set_led(i,0); */
-        /* } */
-
-        /* set_color_led((buf[X]>>1)&0xFF ,(buf[Y]>>1)&0xFF,(buf[Z]>>1)&0xFF); */
-        
-        /* /\* printf("%5d %5d %5d LED:%d\n",abs((buf[X]*2))%255,abs((buf[Y]*2))%255,abs((buf[Z]*2))%255,id); *\/ */
-        
-        
-        /* printf("%5d %5d %5d\n", */
-        /*        buf[X],buf[Y],buf[Z]); */
-        /* /\* printf("\033[%dA" ,1); //カーソルを上に移動 *\/ */
-        
-        /* /\* set_color_led(buf[X]+0x7F,buf[Y]+0x7F,buf[Z]+0x7F); *\/ */
-        
-        
-
-        /* delay_ms(10); */
-    /* } */
     
     
     return 0;
